4f88a7e7ac12c1987a0754394771f7a98a20af78
so6er <mrkvktrvch@gmail.com>
2021-11-02 21:51:25 +0300

    finished test

---
+++ src/test/java/gmail/mrkvktrvch/FindCodeExampleTest.java
@@ -0,0 +1,27 @@
+package gmail.mrkvktrvch;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FindCodeExampleTest {
+
+    @Test
+    void shouldBeCodeExampleForJunit5 () {
+
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(withText("more pages")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $$("ol").first().shouldHave(text("JUnit5 extension - com.codeborne.selenide.junit5.SoftAssertsExtension"));
+
+    }
+
+}
